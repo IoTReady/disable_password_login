fcef282cd902bb6f42841a26b0264fe46cdd169a
Tej Pochiraju <tej@iotready.co>
2022-10-15 13:53:35 +0530

    Move function to tasks.py

--- disable_password_login/hooks.py
+++ disable_password_login/hooks.py
@@ -1,4 +1,3 @@
-import frappe
 from . import __version__ as app_version
 
 app_name = "disable_password_login"
@@ -7,20 +6,6 @@ app_publisher = "IoTReady"
 app_description = "Disallow Password Login If Social Login Is Enabled"
 app_email = "hello@iotready.co"
 app_license = "MIT"
-
-
-def disable_password(doc, event):
-    assert doc.doctype == 'User', "Only valid for User doctype"
-    if not frappe.db.count('Social Login Key', filters={'enable_social_login': 1}) > 0:
-        return False
-    config = frappe.get_doc('Disable Password Login Settings', '')
-    if not config.disable_password_login:
-        return False
-    allowed_users = [row.user for row in config.allow_password_login_table]
-    if doc.name == 'Administrator' or doc.name in allowed_users:
-        return False
-    doc.password = None
-    return True
         
 
 # Document Events
@@ -29,6 +14,6 @@ def disable_password(doc, event):
 
 doc_events = {
     "User": {
-        "on_update": "disable_password_login.hooks.disable_password",
+        "on_update": "disable_password_login.tasks.disable_password",
     }
 }
---
+++ disable_password_login/tasks.py
@@ -0,0 +1,15 @@
+import frappe
+
+
+def disable_password(doc, event):
+    assert doc.doctype == 'User', "Only valid for User doctype"
+    if not frappe.db.count('Social Login Key', filters={'enable_social_login': 1}) > 0:
+        return False
+    config = frappe.get_doc('Disable Password Login Settings', '')
+    if not config.disable_password_login:
+        return False
+    allowed_users = [row.user for row in config.allow_password_login_table]
+    if doc.name == 'Administrator' or doc.name in allowed_users:
+        return False
+    doc.password = None
+    return True
